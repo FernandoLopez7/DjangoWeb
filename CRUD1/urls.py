86137413efa3a2414705e90101aeef734042e841
FernandoLopez7 <fernando.lopez.achig@udla.edu.ec>
2023-06-03 23:06:16 -0500

    Pulir codigo 1

--- CRUD1/urls.py
+++ CRUD1/urls.py
@@ -4,23 +4,23 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+
     # CRUD1
     path('usercreate', views.usercreate, name='usercreate'),
     path('userdetails/<int:id>', views.userdetails, name='userdetails'),
     path('useredit/<int:id>', views.useredit, name='useredit'),
     path('userdelete/<int:id>', views.userdelete, name='userdelete'),
-    
+
     # Login, logout and register
-    path('logout/',views.exit, name='exit'),
-    path('iniciarsesion/',views.login_or_register, name='login_or_register'),
-    
+    path('logout/', views.exit, name='exit'),
+    path('iniciarsesion/', views.login_or_register, name='login_or_register'),
+
     # Mini core
-    path('clientes/',views.indexclientes, name='clientes'),
-    path('clientes/create/',views.clientescreate, name='clientescreate'),
-    
-    path('contratos/',views.indexcontratos, name='contratos'),
-    path('contratos/create/',views.contratoscreate, name='contratoscreate'),
-    
+    path('clientes/', views.indexclientes, name='clientes'),
+    path('clientes/create/', views.clientescreate, name='clientescreate'),
+
+    path('contratos/', views.indexcontratos, name='contratos'),
+    path('contratos/create/', views.contratoscreate, name='contratoscreate'),
+
     path('reporte/', views.report, name='reporte'),
 ]
