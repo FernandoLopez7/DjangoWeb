460310e14accb3e0758805814e74c5cbb2372361
FernandoLopez7 <fernando.lopez.achig@udla.edu.ec>
2023-07-01 14:27:26 -0500

    CORE and MiniCore

--- CRUD1/form.py
+++ CRUD1/form.py
@@ -5,19 +5,18 @@ from django.contrib.auth.models import User
 
 # Esto era para el form de las tablas creadas
 from django.forms import ModelForm
-from .models import Usuario, Cliente, Contrato
+from .models import Usuario, Vendedor, Producto, Venta
 
 # CRUD
-
-
+##########################################################################################
 class UsuarioForm(ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
+##########################################################################################
 
 # Login, logout and register
-
-
+##########################################################################################
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
     # contraseña1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
@@ -33,32 +32,50 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+##########################################################################################
+
 
 # Mini core
-
-
-class ClienteForm(ModelForm):
+##########################################################################################
+class VendedorForm(ModelForm):
     class Meta:
-        model = Cliente
+        model = Vendedor
+        fields = '__all__'
+
+class ProductoForm(ModelForm):
+    class Meta:
+        model = Producto
         fields = '__all__'
 
 
-class ContratoForm(ModelForm):
-    # cliente = forms.ModelChoiceField(queryset=Cliente.objects.all(), label='Cliente')
-    cliente = forms.ModelChoiceField(
-        queryset=Cliente.objects.all(),
-        label='Cliente',
+class VentaForm(ModelForm):
+    vendedor = forms.ModelChoiceField(
+        queryset=Vendedor.objects.all(),
+        label='Vendedor',
+        to_field_name='nombre',  # aquí especificamos el campo 'nombre' en lugar del campo 'id'
+        widget=forms.Select(attrs={'class': 'form-control'}),
+    )
+    producto = forms.ModelChoiceField(
+        queryset=Producto.objects.all(),
+        label='Producto',
         to_field_name='nombre',  # aquí especificamos el campo 'nombre' en lugar del campo 'id'
         widget=forms.Select(attrs={'class': 'form-control'}),
     )
     class Meta:
-        model = Contrato
-        fields = ['cliente', 'descripcion', 'precio']
-        widgets = {
-            'cliente': forms.Select(),
-            # 'fecha_edicion': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-        }
+        model = Venta
+        fields = ['vendedor', 'producto', 'cantidad']
+  
+    def save(self, commit=True):
+        venta = super().save(commit=False)
 
+        cantidad = self.cleaned_data['cantidad']
+        producto = self.cleaned_data['producto']
+        venta.monto = cantidad * producto.precioUnitario
+
+        if commit:
+            venta.save()
+
+        return venta
 
 class DateFilterForm(forms.Form):
     fecha_inicio = forms.DateField(
@@ -82,3 +99,31 @@ class DateFilterForm(forms.Form):
                 raise forms.ValidationError(
                     'La fecha de inicio no debe ser mayor que la fecha de fin')
         return cleaned_data
+##########################################################################################
+
+# CORE
+##########################################################################################
+class DatosVentaUsuarioForm(forms.Form):
+    conjunto_id = forms.CharField(max_length=10)
+    datos_file = forms.FileField()
+    
+class PrediccionVentaForm(forms.Form):
+    OPCIONES_MODELO = [
+        ('sarima', 'Sarima'),
+        ('ols', 'Ordinary Least Squares (OLS)'),
+    ]
+        
+    conjunto_id = forms.CharField(max_length=10)
+    fecha_inicio = forms.DateField(widget=forms.DateInput(attrs={'class': 'datepicker', 'type': 'date'}))
+    fecha_fin = forms.DateField(widget=forms.DateInput(attrs={'class': 'datepicker', 'type': 'date'}))
+    modelo_usado = forms.ChoiceField(choices=OPCIONES_MODELO, label="Modelo usado")
+    
+    class Meta:
+        labels = {
+            'conjunto_id': 'Identificador de datos',
+            'fecha_inicio': 'Fecha de inicio',
+            'fecha_fin': 'Fecha de fin',
+            'modelo_usado':'Modelo usado',
+        }
+
+##########################################################################################
--- CRUD1/urls.py
+++ CRUD1/urls.py
@@ -16,11 +16,25 @@ urlpatterns = [
     path('iniciarsesion/', views.login_or_register, name='login_or_register'),
 
     # Mini core
-    path('clientes/', views.indexclientes, name='clientes'),
-    path('clientes/create/', views.clientescreate, name='clientescreate'),
+    path('vendedores/', views.indexvendedor, name='vendedores'),
+    path('vendedores/create/', views.vendedorescreate, name='vendedorescreate'),
 
-    path('contratos/', views.indexcontratos, name='contratos'),
-    path('contratos/create/', views.contratoscreate, name='contratoscreate'),
+    path('productos/', views.indexproducto, name='productos'),
+    path('productos/create/', views.productoscreate, name='productoscreate'),
+
+    path('ventas/', views.indexventas, name='ventas'),
+    path('ventas/create/', views.ventascreate, name='ventascreate'),
 
     path('reporte/', views.report, name='reporte'),
+    
+    # Test
+    # path('mostrar_user_id/', views.mostrar_user_id, name='mostrar_user_id'),
+    
+    # Core
+    path('indexCore/<int:id>/', views.tabla_datos_venta, name='indexCore'),
+    path('crear-datos-venta/', views.datos_venta_usuario_create, name='crear_datos_venta'),
+    path('realizar_prediccion/', views.realizar_prediccion, name='realizar_prediccion'),
+    path('grafica_ventas/<str:id>/', views.grafica_datos_venta, name='grafica_ventas'),
+    path('grafica_pronostico/<int:id>/', views.grafica_datos_prediccion, name='grafica_pronostico'),
+    path('calculo', views.calculo, name='calculo'),
 ]
